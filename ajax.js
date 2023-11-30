51bc98db53cfe8b87f9e46095ce1e5adfe14c6f1
Shadow <tomascharouzd@gmail.com>
2023-11-30 20:59:44 +0100

    Add files via upload

---
+++ ajax.js
@@ -0,0 +1,26 @@
+<script type="text/javascript">
+function chat() {
+    $.ajax({
+      method: "POST",
+      url: "data.php",
+      data: {
+        input: $("#input").val(),
+        name: $("#name").val()
+      }
+    }).done(function(result) {
+      $("#chat").html(result)
+      $("#input").val("")
+    })
+}
+$("#btn").click(function() {
+    chat()
+})
+setInterval(function() {
+    $.ajax({
+        method: "POST",
+        url: "data.php"
+      }).done(function(result) {
+        $("#chat").html(result)
+      })
+}, 100)
+</script>
---
+++ index.php
@@ -0,0 +1,16 @@
+setInterval function() 
+<?php
+if (isset($_POST['input'])) {
+    $text = trim($_POST['input']);
+    if ($text != "") {
+        $f = fopen("chat.txt", "a");
+        fwrite($f, $text."\n");
+        fclose($f);
+        echo file_get_contents("chat.txt");
+    }
+}
+else {
+    echo file_get_contents("chat.txt");
+}
+<textarea class="form-control mb-3" rows="20" id="chat" readonly></textarea>
+?>
