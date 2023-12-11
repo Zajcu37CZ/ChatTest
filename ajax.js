db6900c73564b50dde0ec36f92a8dfbcc7d256eb
Shadow <tomascharouzd@gmail.com>
2023-12-11 05:39:09 +0100

    Update ajax.js
    fixing chat

--- ajax.js
+++ ajax.js
@@ -1,24 +1,24 @@
 function chat() {
-    $.ajax({
-      method: "POST",
-      url: "data.php",
-      data: {
-        input: $("#input").val(),
-        name: $("#name").val()
-      }
-    }).done(function(result) {
-      $("#chat").html(result)
-      $("#input").val("")
-    })
+  $.ajax({
+    method: "POST",
+    url: "index.php",
+    data: {
+      input: $("#input").val(),
+      name: $("#name").val()
+    }
+  }).done(function(result) {
+    $("#chat").html(result)
+    $("#input").val("")
+  })
 }
 $("#btn").click(function() {
-    chat()
+  chat()
 })
 setInterval(function() {
-    $.ajax({
-        method: "POST",
-        url: "data.php"
-      }).done(function(result) {
-        $("#chat").html(result)
-      })
+  $.ajax({
+      method: "POST",
+      url: "index.php"
+    }).done(function(result) {
+      $("#chat").html(result)
+    })
 }, 100)
